40c95fc2d4d125c99e1914a97a6306a030205750
Jay Jung <cj329@cornell.edu>
2014-10-30 21:11:25 -0400

    warmup

--- ps5/async/warmup.ml
+++ ps5/async/warmup.ml
@@ -1,7 +1,12 @@
 open Async.Std
 
-let fork d f1 f2 =
-  failwith "My mind is aglow with whirling, transient nodes of thought..."
-
-let deferred_map l f =
-  failwith "Ditto."
+let fork (d :'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2 : 'a -> 'c Deferred.t) : unit =
+  ignore (d >>= fun v ->
+  	return ((f1 v), (f2 v))) 
+    
+let deferred_map (l: 'a list) (f: 'a -> 'b Deferred.t): 'b list Deferred.t =
+  let fold_help a acc =
+    (f a) >>= fun v -> 
+    acc >>= fun lst ->
+      return (v::lst) in
+  List.fold_right (fold_help) l (return [])
