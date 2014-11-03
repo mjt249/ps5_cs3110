ee3dc6d43b92e1240bf8c48c748a6afe9f6820eb
sheroze1123 <mss385@cornell.edu>
2014-11-03 15:39:27 -0500

    Working worker

--- ps5/map_reduce/worker.ml
+++ ps5/map_reduce/worker.ml
@@ -1,11 +1,40 @@
 open Async.Std
+open Protocol
 
 module Make (Job : MapReduce.Job) = struct
 
-  (* see .mli *)
-  let run r w =
-    failwith "Nowhere special?  I always wanted to go there."
+  module JobResponse = Protocol.WorkerResponse(Job)
+  module JobRequest = Protocol.WorkerRequest(Job)
 
+  (* see .mli *)
+  let run (r : Reader.t) (w : Writer.t) : unit Deferred.t =
+    JobRequest.receive r >>= (fun res -> 
+      match res with
+      | `Eof -> failwith "Error, pipe is closed."
+      | `Ok (JobRequest.MapRequest input) -> 
+          let work = Job.map input in
+          try_with (fun () -> work) 
+          >>| (function
+          | Core.Std.Ok determined_work -> 
+              let response  = JobResponse.(MapResult determined_work) in
+              JobResponse.send w response; 
+              don't_wait_for (Reader.close r)
+          | Core.Std.Error ex -> 
+              let response = JobResponse.(JobFailed (Printexc.to_string ex)) in
+              JobResponse.send w response;
+              don't_wait_for (Reader.close r))
+      | `Ok (JobRequest.ReduceRequest (key, lst)) -> 
+          let work = Job.reduce (key, lst) in
+          try_with (fun () -> work) 
+          >>| (function
+          | Core.Std.Ok determined_work -> 
+              let response  = JobResponse.(ReduceResult determined_work) in
+              JobResponse.send w response; 
+              don't_wait_for (Reader.close r)
+          | Core.Std.Error ex -> 
+              let response = JobResponse.(JobFailed (Printexc.to_string ex)) in
+              JobResponse.send w response;
+              don't_wait_for (Reader.close r)))
 end
 
 (* see .mli *)
