b9f255e417813a428057ced1acf4d4be7adeffa4
Jay Jung <cj329@cornell.edu>
2014-11-05 17:58:31 -0500

    order

--- ps5/map_reduce/remoteController.ml
+++ ps5/map_reduce/remoteController.ml
@@ -27,35 +27,45 @@ module Make (Job : MapReduce.Job) = struct
 
   let map_reduce inputs = 
     let idle_worker_aQueue = create () in
-    let mapping_worker_aQueue = create () in
+    let busy_worker_aQueue = create () in
     let map_response = ref (return []) in
     let number_of_inputs_mapped = ref 0 in
     let shuffled_maps = ref (return []) in
-    let ready_for_reduce = ref false in
+    let number_of_keys = ref 0 in
+    let number_of_pairs_reduced = ref 0 in
+    let reduced_result = ref (return []) in
 
-
-    don't_wait_for(
+    let connect_workers : unit -> unit Deferred.t = fun () ->
     (*connects workers*)
-    Deferred.List.map ~how:`Parallel (!addresses) ~f:Tcp.connect >>= fun connected_workers ->
+      Deferred.List.map ~how:`Parallel (!addresses) ~f:Tcp.connect >>= 
+        fun connected_workers ->
     (*enqueues connected workers into aQueue*)
-    Deferred.List.iter ~how:`Parallel connected_workers ~f:(fun x -> return (push idle_worker_aQueue x)));
+        Deferred.List.iter ~how:`Parallel connected_workers ~f:(fun x -> 
+        	return (push idle_worker_aQueue x)) in
+
+    let send_map_requests : unit -> unit Deferred.t = fun () ->
     (*sending map requests and enqueueing map workers*)
     Deferred.List.iter ~how:`Parallel inputs ~f:(fun input ->
      return ( (pop worker_aQueue) >>= fun (s, r, w) -> 
-    	Request.send w (Request.MapRequest input); push mapping_worker_aQueue (s, r, w));
+    	Request.send w (Request.MapRequest input); push busy_worker_aQueue (s, r, w))) in
     (*read map results from busy workers*)
+
+    let send_reduce_requests : unit -> unit Deferred.t = fun () ->
+      Deferred.List.iter ~how:`Parallel (!shuffuled_maps) ~f:(fun (key, lst) -> 
+      	return ( (pop idle_worker_aQueue) >>= fun (s, r, w) -> 
+    	  Request.send w (Request.ReduceRequest pair); push busy_worker_aQueue (key ,(s, r, w))) in
     
 
-    let rec read_mapping =
-      if (number_of_inputs_mapped = List.length(inputs)) then
+    let rec read_map_response : unit -> unit Deferred.t = fun () ->
+      if ((!number_of_inputs_mapped) = List.length(inputs)) then
         map_response >>= fun res_list ->
         (*shuffule map_response!!*)
-        shuffuled_maps := ((!map_response) 
+        shuffled_maps := ((!map_response) 
                           >>| Combine.combine);
-        ready_for_reduce := true;
+        number_of_keys := List.length(!shuffuled_maps);
         return ()
       else
-        pop mapping_worker_aQueue >>= fun (s, r, w) -> 
+        pop busy_worker_aQueue >>= fun (s, r, w) -> 
           Response.receive r >>= fun res -> function
             (`Ok (MapResult pairs)) -> 
               map_response >>= fun mappings -> 
@@ -66,20 +76,36 @@ module Make (Job : MapReduce.Job) = struct
               (*number of inputs successfully mapped and added*)
               number_of_inputs_mapped := number_of_inputs_mapped + 1;
               push idle_worker_aQueue (s, r, w);
-              read_mapping
+              read_map_response
           (*will be JobFailed because this Queue only had map requests.
             need a way to find out which input JobFailes came from before I can send the input to another worker.*)
-            | _ -> read_mapping in
+            | _ -> read_map_response in
 
-    let rec reduce_mappings 
+    let rec read_reduce_response: unit -> unit Deferred.t = fun () ->
+      if ((!number_of_pairs_reduced) = (!number_of_keys)) then 
+        return ()
+      else
+        pop busy_worker_aQueue >>= fun (key, (s, r, w)) -> 
+          Response.receive r >>= fun res -> function
+            (`Ok (ReduceResult output)) -> 
+              reduced_result >>= fun results -> 
+              (*adding response pairs to list*)
+              (*I'm flattening the list here already. should I not flatten and do
+              >>| List.flatten later in the if clause above?*)
+              results := ((key, output)::results);
+              (*number of inputs successfully mapped and added*)
+              number_of_pairs_reduced := number_of_pairs_reduced + 1;
+              push idle_worker_aQueue (s, r, w);
+              read_reduce_response
+          (*will be JobFailed because this Queue only had map requests.
+            need a way to find out which input JobFailes came from before I can send the input to another worker.*)
+            | _ -> read_reduce_response in
 
-    don't_wait_for (read_mapping);
-
-
-
-
-
-
+    connect_workers () >>=
+    send_map_requests >>=
+    read_map_response >>=
+    send_reduce_requests >>=
+    send_reduce_requests >>=
     
 end
 
