4e2130288d5455d9fc895591b5d7569da5f4de2b
Jay Jung <cj329@cornell.edu>
2014-11-08 23:06:15 -0500

    squished sends and reads together

--- ps5/map_reduce/remoteController.ml
+++ ps5/map_reduce/remoteController.ml
@@ -28,124 +28,126 @@ module Make (Job : MapReduce.Job) = struct
   let map_reduce inputs = 
 
     let idle_workers = create () in
+    let num_idle = ref 0 in
+
     let map_workers = create () in
+    let num_mapper = ref 0 in
+
     let reduce_workers = create () in
+    let num_reducer = ref 0 in
 
     let map_result = ref (return []) in
-    let number_of_inputs_mapped = ref 0 in
 
     let combined_result = ref (return []) in
     
-    let number_of_keys = ref 0 in
-    let number_of_pairs_reduced = ref 0 in
-    
     let reduced_result = ref (return []) in
 
-    let workers_down = ref 0 in
+    let input_q = create () in
+    let num_input = ref (List.length(inputs)) in
+
+    let key_q = create () in
+    let num_keys = ref 0 in
 
     let connect_wrapper worker_id =
       try_with (fun () -> (Tcp.connect worker_id)) 
         >>| (function
-        | Core.Std.Ok connected -> push idle_workers (worker_id, connected)
-        | Core.Std.Error _ -> workers_down := !workers_down + 1;
-            () ) in
+        | Core.Std.Ok connected -> push idle_workers (worker_id, connected);
+                                   num_idle := !num_idle +1 
+        | Core.Std.Error _ -> () ) in
     
     let connect_workers : unit -> unit Deferred.t = fun () ->
     (*connects workers and enqueues them*)
       Deferred.List.iter ~how:`Parallel (!worker_ides) ~f: connect_wrapper in
 
+    let enqueue_inputs : unit -> unit Deferred.t = fun () ->
+      Deferred.List.iter ~how:`Parallel inputs ~f: fun input -> return(push input_q input ) in
+
     let map_helper input = function
       (worker_id, (s, r, w)) -> 
           Request.send w (Request.MapRequest input); 
+          num_mapper := !num_mapper +1;
           return (push map_workers (input, (worker_id, (s, r, w)))) in
-(*       (worker_id, connected) -> 
-        connected >>= fun (s, r, w) ->
-          Request.send w (Request.MapRequest input); 
-          return (push map_workers (input, (worker_id, (s, r, w))))
-      | _ -> failwith "map_helper is confused... and I'm confused." in *)
-
-    let send_map_requests : unit -> unit Deferred.t = fun () ->
-    (*sending map requests and enqueueing map workers*)
-    Deferred.List.iter ~how:`Parallel inputs ~f:(fun input ->
-    	(*pop idle_workers has type (worker_id, ((s,r,w) deferred.t)) deferred.t *)
-     (pop idle_workers) >>= (map_helper input) ) in
-      		
     
-    let rec read_map_response : unit -> unit Deferred.t = fun () ->
-      if ((!workers_down) = List.length(!worker_ides)) then
+    let rec map_phase: unit -> unit Deferred.t = fun () ->
+      if ((!num_idle = 0) && (!num_mapper = 0)) then
         raise InfrastructureFailure
-      else if ((!number_of_inputs_mapped) = List.length(inputs)) then
+      else if ((!num_input = 0) && (!num_mapper = 0)) then
         !map_result >>= fun res_list ->
         (*shuffle map_result!!*)
         combined_result := ((!map_result) 
         	              >>| List.flatten
                           >>| Combine.combine);
         !combined_result >>= fun num ->
-        number_of_keys := List.length(num);
+        num_keys := List.length(num);
         return ()
-      else
+      else if ((!num_input > 0) && (!num_idle > 0)) then 
+        pop input_q >>= fun input ->
+        pop idle_workers >>= map_helper input 
+        >>= fun _ -> 
+        num_idle := !num_idle -1;
+        num_input := !num_input -1;
+        map_phase ()
+      else if ((!num_input = 0) || (!num_idle = 0)) then
         pop map_workers >>= fun (input, (worker_id, (s, r, w))) -> 
+          num_mapper := !num_mapper - 1;
           (Response.receive r)>>= function 
             |(`Ok (Response.MapResult key_inter_pair_list)) -> 
               !map_result >>= fun mappings -> 
-              (*adding response pairs to list*)
-              (*I'm flattening the list here already. should I not flatten and do
-              >>| List.flatten later in the if clause above?*)
               map_result := return(key_inter_pair_list::mappings);
-(*               map_result := Deferred.List.fold ~how:`Parallel mappings key_inter_pair_list ~f:(fun acc el -> return (el::acc)); *)
-              (*number of inputs successfully mapped and added*)
-              number_of_inputs_mapped := !number_of_inputs_mapped + 1;
               connect_wrapper worker_id >>= fun _ ->
-              read_map_response ()
+              map_phase ()
             |(`Ok Response.ReduceResult _ )-> failwith "map queue is confused... it's reducing" 
-            | _ -> workers_down := !workers_down +1;
-                   ((pop idle_workers) >>= (map_helper input)) >>= fun _ ->
-                   read_map_response () in
+            | _ -> push input_q input;
+                   num_input := !num_input +1;
+                   map_phase()
+       else
+         failwith "map gone wrong" in
 
+
+    let enqueue_keys : unit -> unit Deferred.t = fun () ->
+      !combined_result >>= fun lst ->
+      Deferred.List.iter ~how:`Parallel lst ~f: fun el -> return(push key_q el) in
 
     let reduce_helper key lst = function
       (worker_id, (s, r, w)) ->
     	  Request.send w (Request.ReduceRequest (key, lst) );
+          num_reducer := !num_reducer +1;
     	  return (push reduce_workers (key, lst, (worker_id, (s, r, w)))) in
-(*       (worker_id, connected) ->
-        connected >>= fun (s, r, w) -> 
-    	  Request.send w (Request.ReduceRequest (key, lst) );
-    	  return (push reduce_workers (key, lst, (worker_id, (s, r, w))))
-      | _ -> failwith "reduce_helper is confused too :( " in *)
 
-    
-    let send_reduce_requests : unit -> unit Deferred.t = fun () ->
-      !combined_result >>= fun comb_lst ->
-      Deferred.List.iter ~how:`Parallel (comb_lst) ~f:(fun (key, lst) ->
-        (pop idle_workers) >>= (reduce_helper key lst)) in
-
-    let rec read_reduce_response : unit -> (Job.key * Job.output) list Deferred.t = fun () ->
-      if ((!workers_down) = List.length(!worker_ides)) then
+    let rec reduce_phase : unit -> (Job.key * Job.output) list Deferred.t = fun () ->
+      if ((!num_idle = 0) && (!num_reducer = 0)) then
         raise InfrastructureFailure
-      else if ((!number_of_pairs_reduced) = (!number_of_keys)) then 
+      else if ((!num_keys = 0) && (!num_reducer = 0)) then 
         !reduced_result
-      else
+      else if ((!num_keys > 0) && (!num_idle > 0)) then 
+        pop key_q >>= fun (key, lst) ->
+        pop idle_workers >>= (reduce_helper key lst)
+        >>= fun _ -> 
+        num_idle := !num_idle -1;
+        num_keys := !num_keys -1;
+        reduce_phase ()
+      else if ((!num_keys = 0) || (!num_idle = 0)) then
         pop reduce_workers >>= fun (key, lst, (worker_id, (s, r, w))) -> 
           Response.receive r >>= function
             |(`Ok (Response.ReduceResult output)) -> 
               !reduced_result >>= fun results -> 
               (*adding response pairs to list*)
               reduced_result := return((key, output)::results);
-              (*number of inputs successfully mapped and added*)
-              number_of_pairs_reduced := !number_of_pairs_reduced + 1;
               connect_wrapper worker_id >>= fun _ ->
-              read_reduce_response ()
+              reduce_phase ()
           (*will be JobFailed because this Queue only had reduce requests.
             need a way to find out which input JobFailes came from before I can send the input to another worker.*)
             |(`Ok Response.MapResult _ )-> failwith "reduce queue is confused... it's mapping" 
-            | _ -> workers_down := !workers_down +1;
-                  ((pop idle_workers) >>= (reduce_helper key lst)) >>= fun _ ->
-                  read_reduce_response () in
+            | _ -> push key_q (key, lst);
+                   num_keys := !num_keys +1;
+                   reduce_phase()
+       else
+         failwith "reduce gone wrong" in
 
     connect_workers () >>=
-    send_map_requests >>=
-    read_map_response >>=
-    send_reduce_requests >>=
-    read_reduce_response 
+    enqueue_inputs >>=
+    map_phase >>=
+    enqueue_keys >>=
+    reduce_phase
 end
 
