942b47c86e2b3ec92a4059dbf2643187571ee10e
Jay Jung <cj329@cornell.edu>
2014-11-13 13:40:13 -0500

    final warmup

--- ps5/async/warmup.ml
+++ ps5/async/warmup.ml
@@ -5,9 +5,10 @@ let fork (d :'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2 : 'a -> 'c Deferred.t)
   	return ((f1 v), (f2 v))) 
     
 let deferred_map (l: 'a list) (f: 'a -> 'b Deferred.t): 'b list Deferred.t =
-  let fold_help a acc =
+  let fold_help acc a=
     (f a) >>= fun v -> 
     acc >>= fun lst ->
       return (v::lst) in
-  List.fold_right (fold_help) l (return [])
+  List.fold_left (fold_help) (return []) l 
+  >>| List.rev
 
