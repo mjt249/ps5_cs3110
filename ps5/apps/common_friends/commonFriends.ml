f84687e4354adfe3fb0fa75fe9acaefbcd6c82c8
Connor Daly <cgd37@cornell.edi>
2014-11-13 12:22:14 -0500

    Updated connor branch

--- ps5/apps/common_friends/commonFriends.ml
+++ ps5/apps/common_friends/commonFriends.ml
@@ -1,95 +1,94 @@
 open Async.Std
-
 module Job = struct
-  type input  = string * string list
-  type key    = string * string
-  type inter  = string list
+  type input = string * string list
+  type key = string * string
+  type inter = string list
   type output = string list
-
-  let rec find_other_nodes friendlist node = 
+  (*
+   * Takes a list of nodes and a particular node. Returns the original list without
+   * all elements matching the input node.
+   * Args : a string list, friendlist, and a node of type string.
+   * Preconditions : function must typecheck
+   * Postconditions: Output is a list of type inter matching friendlist not containing node
+   *)
+  let rec find_other_nodes (friendlist: string list) (node: string) : inter =
     match friendlist with
     | [] -> []
     | hd::tl when hd <> node -> hd::(find_other_nodes tl node)
     | hd::tl -> find_other_nodes tl node
-
-  let rec compute_pairs name friendlist =
-    match friendlist with
-    | [] -> []
-    | hd::tl when compare name hd = -1 -> 
-       ((name, hd), find_other_nodes friendlist hd)::(compute_pairs name tl)
-    | hd::tl -> ((hd, name), find_other_nodes friendlist hd)::(compute_pairs name tl)
-
-  let name = "friends.job"
-  (* string * string list -> (key * inter) list Deferred.t  
-     gets passed name of root node and list of all connected nodes 
-     should I compute all pairs?
+  (*
+   * Takes a tuple of a string, and two identical string lists. Outputs a list of key, inter
+   * tuples corresponding to a key of the string matched with every element in the string
+   * list with each combination's inter being a list of all the other strings in the string
+   * list.
+   * Args : a string, name, and two identical string lists, friendlist_complete
+   * and friendlist_partial
+   * Preconditions : friendlist_partial and friendlist_complete must be identical
+   * Postconditions: Output is a list of key inter tuples. Each tuple contains name matched
+   * with one element from one of the friendlists and the inter is a list of
+   * the rest of the elements in friendlist
    *)
-  let map (name, friendlist) =
-    let sorted_list = List.fast_sort compare friendlist in 
-    return (compute_pairs name sorted_list)
-
-  let rec reduce_helper (pair, friendlists) = 
-    (* inter list should only ever have length 2 *)
+  let rec compute_pairs (name, friendlist_complete, friendlist_partial) : (key * inter) list =
+    match friendlist_partial with
+    | [] -> []
+    | hd::tl when compare name hd = -1 ->
+       ((name, hd), (find_other_nodes friendlist_complete hd))::(
+	compute_pairs (name, friendlist_complete, tl))
+    | hd::tl -> ((hd, name), find_other_nodes friendlist_complete hd)::(
+	       compute_pairs (name, friendlist_complete, tl))
+  let name = "friends.job"
+  let map (name, friendlist): (key * inter) list Deferred.t =
+    let sorted_list = List.fast_sort compare friendlist in
+    return (compute_pairs (name, sorted_list, sorted_list))
+  (* Performs the exact same as reduce, but output is of type output, not output Deferred.t *)
+  let rec reduce_helper (pair, (friendlists: string list list)) : output=
     match friendlists with
     | [] -> []
     | []::[] -> []
-    | [hd::tl]::[] -> []
-    | []::[hd::tl] -> []
-    | [hd::tl]::[x::xs]::[] -> if compare hd x = 0 then hd::(reduce_helper (pair, [tl]::[[xs]]))
-			   else if compare hd x = -1 then reduce_helper (pair, [tl]::[[x::xs]])
-			   else reduce_helper (pair, [hd::tl]::[[xs]])
+    | hd::[[]] -> []
+    | []::hd::[] -> []
+    | (hd::tl)::(x::xs)::[] -> if compare hd x = 0 then hd::(reduce_helper (pair, (tl::[xs])))
+			       else (if compare hd x = -1 then reduce_helper (pair, (tl::[x::xs]))
+				     else reduce_helper (pair, ((hd::tl)::[xs])))
     | _ -> failwith "Invalid list size"
-  (* (key * inter list) -> string list Deferred.t 
-     should take in lists of pairs and see if they match any other pair or something
-   *)
-  let reduce (pair, friendlists) =
+  let reduce ((pair: key), (friendlists: inter list)) : output Deferred.t =
     return (reduce_helper (pair, friendlists))
-      
-
 end
-
 let () = MapReduce.register_job (module Job)
-
 let read_line (line:string) :(string * (string list)) =
   match Str.split (Str.regexp ":") line with
-    | [person; friends] -> begin
-      let friends = Str.split (Str.regexp ",") friends in
-      let trimmed = List.map String.trim friends in
-      (person, trimmed)
-    end
-    | _ -> failwith "Malformed input in graph file."
-
+  | [person; friends] -> begin
+			 let friends = Str.split (Str.regexp ",") friends in
+			 let trimmed = List.map String.trim friends in
+			 (person, trimmed)
+		       end
+  | _ -> failwith "Malformed input in graph file."
 let read_files (files: string list) : ((string * (string list)) list) Deferred.t =
   match files with
-  | []    -> failwith "No graph files provided."
+  | [] -> failwith "No graph files provided."
   | files -> begin
-    Deferred.List.map files Reader.file_lines
-    >>| List.flatten
-    >>| List.map read_line
-  end
-
+	     Deferred.List.map files Reader.file_lines
+	     >>| List.flatten
+	     >>| List.map read_line
+	   end
 module App = struct
   let name = "friends"
-
   let print common_friends =
     let print_friends ((a, b), friends) =
       printf "(%s, %s): %s\n" a b (String.concat ", " friends)
     in
     List.iter print_friends common_friends
-
   module Make (Controller : MapReduce.Controller) = struct
     module MR = Controller(Job)
-
     (* You may assume that main is called with a single, valid graph file. You
-       do not need to handle malformed input. For example graph files, see the
-       data directory. *)
+do not need to handle malformed input. For example graph files, see the
+data directory. *)
     let main args =
-        read_files args
-        >>= MR.map_reduce
-        (* replace this failwith with print once you've figured out the key and
-           inter types*)
-        >>| fun x -> print x
+      read_files args
+      >>= MR.map_reduce
+      (* replace this failwith with print once you've figured out the key and
+inter types*)
+      >>| fun x -> print x
   end
 end
-
 let () = MapReduce.register_app (module App)
--- ps5/map_reduce/worker.ml
+++ ps5/map_reduce/worker.ml
@@ -1,42 +1,60 @@
 open Async.Std
 open Protocol
-
 module Make (Job : MapReduce.Job) = struct
-
-  module JobRequest = WorkerRequest(Job)
-  module JobResponse = WorkerResponse(Job)
-
+  module JobResponse = Protocol.WorkerResponse(Job)
+  module JobRequest = Protocol.WorkerRequest(Job)
+  let rec compute_single_request (r : Reader.t) (w : Writer.t) : unit Deferred.t =
+    JobRequest.receive r
+    >>= (fun res ->
+	 match res with
+	 | `Eof -> failwith "Reader is closed."
+	 | `Ok (JobRequest.MapRequest input) ->
+	    let work = Job.map input in
+	    try_with (fun () -> work)
+	    >>| (function
+		  | Core.Std.Ok determined_work ->
+		     let response = JobResponse.(MapResult determined_work) in
+		     (JobResponse.send w response)
+		  | Core.Std.Error ex ->
+		     let response = JobResponse.(JobFailed (Printexc.to_string ex)) in
+		     (JobResponse.send w response))
+	 | `Ok (JobRequest.ReduceRequest (key, lst)) ->
+	    let work = Job.reduce (key, lst) in
+	    try_with (fun () -> work)
+	    >>| (function
+		  | Core.Std.Ok determined_work ->
+		     let response = JobResponse.(ReduceResult determined_work) in
+		     (JobResponse.send w response)
+		  | Core.Std.Error ex ->
+		     let response = JobResponse.(JobFailed (Printexc.to_string ex)) in
+		     (JobResponse.send w response)))
   (* see .mli *)
-  let run r w =
-    JobRequest.receive r >>= fun contents -> 
-      match contents with 
-      | `Eof -> failwith "Pipe is closed"
-      | `Ok (JobRequest.MapRequest input) -> 
-	 let mapped = Job.map input in 
-      | `Ok (JobRequest.ReduceRequest (key, inter)) -> return ()
-
+  let run (r : Reader.t) (w : Writer.t) : unit Deferred.t =
+    while (not (Reader.is_closed r)) do
+      don't_wait_for (compute_single_request r w
+		      >>= (fun _ -> return ()))
+    done;
+    return ()
 end
-
 (* see .mli *)
 let init port =
   Tcp.Server.create
     ~on_handler_error:`Raise
     (Tcp.on_port port)
     (fun _ r w ->
-      Reader.read_line r >>= function
-        | `Eof    -> return ()
-        | `Ok job -> match MapReduce.get_job job with
-          | None -> return ()
-          | Some j ->
-            let module Job = (val j) in
-            let module Worker = Make(Job) in
-            Worker.run r w
+     Reader.read_line r >>= function
+			  | `Eof -> return ()
+			  | `Ok job -> match MapReduce.get_job job with
+				       | None -> return ()
+				       | Some j ->
+					  let module Job = (val j) in
+					  let module Worker = Make(Job) in
+					  Worker.run r w
     )
-    >>= fun _ ->
+  >>= fun _ ->
   print_endline "server started";
   print_endline "worker started.";
   print_endline "registered jobs:";
   List.iter print_endline (MapReduce.list_jobs ());
   never ()
-
 
