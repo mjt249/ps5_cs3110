5e0a06f005b40cdc5cb536144879390d451b0a7a
sheroze1123 <mss385@cornell.edu>
2014-11-13 13:53:07 -0500

    correct order warmup

--- ps5/async/warmup.ml
+++ ps5/async/warmup.ml
@@ -1,7 +1,13 @@
 open Async.Std
 
-let fork d f1 f2 =
-  failwith "My mind is aglow with whirling, transient nodes of thought..."
-
-let deferred_map l f =
-  failwith "Ditto."
+let fork (d :'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2 : 'a -> 'c Deferred.t) : unit =
+  ignore (d >>= fun v ->
+    return ((f1 v), (f2 v))) 
+    
+let deferred_map (l: 'a list) (f: 'a -> 'b Deferred.t): 'b list Deferred.t =
+  let fold_help acc a=
+    (f a) >>= fun v -> 
+    acc >>= fun lst ->
+      return (v::lst) in
+  List.fold_left (fold_help) (return []) l 
+  >>| List.rev
